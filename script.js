95451e60a8c7b27caadb3ce151426a2d2acffe47
spkelly <spkelly18@gmail.com>
2017-02-27 10:25:12 -0700

    added html, css and js boilerplate

--- script.js
+++ script.js
@@ -1,4 +1,37 @@
-$('document').ready(function() {
+$('document').ready(() => {
+  'use strict';
 
-    console.log('hello, world');
+  const initSearchResults = (billResults) => {
+    console.log(billResults);
+  }
+
+  const $nameInput = $('#name-input');
+  const $idInput = $('#id-input');
+  const $searchButton = $('button[type="submit"]');
+
+  console.log($searchButton,$nameInput,$idInput);
+
+  $searchButton.click((e) => {
+    e.preventDefault();
+    if ($nameInput.val() || $idInput.val()) {
+      $.ajax({
+        method: 'GET',
+        url:`https://api.legiscan.com/?key=6a2d12a9259d0c661bf3add8d58cd236&op=getBill&id=976525`,
+        success: function(data){
+          initSearchResults(data);
+          console.log(data);
+        },
+        error: function(data){
+          console.log('Error');
+        }
+      })
+    }
+  })
+
+  // const baseURL = `https://api.legiscan.com/?key=${apiKey}`;
+  // const testHeader = $('<h1></h1>');
+  // const body = $('body');
+  //
+  // testHeader.text(baseURL);
+  // body.append(testHeader);
 });
