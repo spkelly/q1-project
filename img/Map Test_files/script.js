2c2b526512a69713c72fd5420308b4c71c8c4cf3
spkelly <spkelly18@gmail.com>
2017-03-03 06:08:46 -0700

    added styling and vote table

--- img/Map Test_files/script.js
+++ img/Map Test_files/script.js
@@ -11,7 +11,6 @@ $('document').ready(() => {
 
     if ($nameInput.val()) {
       options+=$nameInput.val();
-      console.log("the ajax URL: ",apiURL + apiKey + options);
       $.ajax({
         method: 'GET',
         url: (apiURL + apiKey + options),
@@ -28,7 +27,7 @@ $('document').ready(() => {
         error: function(data) {
           console.log('Error');
         }
-      });
+      })
     }
   };
   const makeCollectionItem = (billItem) => {
@@ -40,10 +39,10 @@ $('document').ready(() => {
     const billNumber = $('<li class="sr-billNumber">');
 
     // Modal stuff
-    const billModal = $('<div>').addClass('modal');
+    const billModal =$('<div>').addClass('modal');
+    billModal.attr('id',`${billItem.bill_id}`);
     const modalContent = $('<div>').addClass('modal-content');
 
-    billModal.attr('id', `${billItem.bill_id}`);
     billModal.append(modalContent);
 
     lastAction.text(`Last Action:  [${billItem.last_action_date}] - ${billItem.last_action}`);
@@ -67,16 +66,15 @@ $('document').ready(() => {
 
     for (let i = 0; i < results.length; i++) {
       const collectionItem = makeCollectionItem(results[i]);
-      const billModal = collectionItem.children('div[class=modal]');
-
       collection.append(collectionItem);
-
+      let billModal = collectionItem.children('div[class=modal]');
+      console.log('the bill modal',billModal);
       // add event listener to each bill button
       // make an ajax call to get bill information
-      collectionItem.click((e) => {
+      collectionItem.click(function(e) {
         e.preventDefault();
-
-        // call a function to get bill details
+        console.log(`${collectionItem}: Has been clicked`);
+        // call a function to get a
         $.ajax({
           method: 'GET',
           url: `https://api.legiscan.com/?key=6a2d12a9259d0c661bf3add8d58cd236&op=getBill&id=${billModal.attr('id')}`,
@@ -99,7 +97,6 @@ $('document').ready(() => {
     let header =$('<thead>');
     let headerRow = $('<tr>');
     let body = $('<tbody>');
-
     headerRow.append('<th>Date</th>', '<th>Type</th>, <th>Description</th>');
     header.append(headerRow);
     table.append(header);
@@ -109,45 +106,41 @@ $('document').ready(() => {
       let date = $('<td>').text(`${event.date} at ${event.time}`);
       let type = $('<td>').text(`${event.type}`);
       let desc = $('<td>').text(`${event.description}`);
-
-      trow.append(date, type, desc);
+      trow.append(date,type,desc);
       body.append(trow);
     }
     table.append(body);
+    console.log( 'the table: ',table);
 
     return table;
   };
   const buildVotes = (voteData) => {
+    console.log(voteData);
     let voteTable = $('<table>').addClass('bordered');
     let header =$('<thead>');
     let headerRow = $('<tr>');
     let body = $('<tbody>');
-
     headerRow.append('<th>Date</th>', '<th>Passed</th>, <th>Description</th>');
     header.append(headerRow);
     voteTable.append(header);
-    for (let i = 0; i < voteData.length; i++) {
+    for (var i = 0; i < voteData.length; i++) {
       let trow = $('<tr>').attr('class','white-text');
       let voteEvent = voteData[i];
       let date = $('<td>').text(`${voteEvent.date}`);
+      if(voteEvent.passed === 1){
+        var passed = $('<td>').text(`passed`);
+        trow.addClass('green');
+      }
+      else{
+        var passed = $('<td>').text(`not passed`);
+        trow.addClass('red');
+      }
       let desc = $('<td>').text(`${voteEvent.desc}`);
-      var passed;
-
-      if (voteEvent.passed === 1) {
-        passed = $('<td>').text('passed');
-
-        trow.addClass('green lighten-1');
-      }
-      else {
-        passed = $('<td>').text('not passed');
-
-        trow.addClass('red lighten-1');
-      }
-      trow.append(date, passed, desc);
+      trow.append(date,passed,desc);
       body.append(trow);
+      console.log(voteEvent);
     }
     voteTable.append(body);
-
     return voteTable;
   };
   const createBill = (billData) => {
@@ -165,7 +158,7 @@ $('document').ready(() => {
     bill.textUrl = billData.texts[0].state_link;
     bill.history = [];
 
-    for (let i = 0; i < billData.sponsors.length; i++) {
+    for(var i = 0; i < billData.sponsors.length; i++){
       let currSponsor = billData.sponsors[i];
       console.log("the current sponsor", currSponsor);
       let person = {};
@@ -177,39 +170,28 @@ $('document').ready(() => {
 
       bill.sponsors.push(person);
     }
-
     return bill;
   };
-  const mapBillStatus = (statusNum) => {
-    const status = $('<p>');
-
+  const mapBillStatus =(statusNum) =>{
+    let status = $('<p>');
     switch (statusNum) {
       case 1:
         status.text('Status: Introduced').addClass('introduced');
         break;
       case 2:
         status.text('Status: Engrossed').addClass('engrossed');
-        break;
-      case 3:
-        status.text('Status: Enrolled').addClass('signed');
-        break;
-      case 4:
-        status.text('Status: Signed').addClass('signed');
-        break;
-      case 5:
-        status.text('Status: Vetoed').addClass('failed');
-        status.attr('color', 'red');
+        status.attr('color', 'yellow');
         break;
       case 6:
         status.text('Status: Failed').addClass('failed');
+        status.attr('color', 'red');
         break;
       default:
-        status.text('Status: Unknown');
-        status.attr('color', 'grey');
+      status.text('Status: Unknown');
+      status.attr('color', 'grey');
     }
-
     return status;
-  };
+  }
   const populateModal = (modalContent, billData) => {
     // remove previous modal elements
     modalContent.empty();
@@ -227,8 +209,7 @@ $('document').ready(() => {
     const exitButton = $('<button>').addClass('btn').text('EXIT');
     const fullTextLink = $('<a>').text('Full Text').attr({
       href: billData.textUrl,
-      target: "_blank",
-      class: 'btn white-text'
+      class: 'btn'
     });
     const billStatus = mapBillStatus(billData.status);
 
@@ -243,33 +224,29 @@ $('document').ready(() => {
     let voteCollapse = $('<li>');
 
     // sponsors
-    let sponsorTitle = $('<h5>').text("Bill sponsors").attr('class','col l3 m3 s12');
-    firstRowDiv.append(sponsorTitle);
+
     for (var i = 0; i < billData.sponsors.length; i++) {
       let sponsorElement = $('<a>').attr({
         class: 'col l12 m12 s12',
-        href: `https://ballotpedia.org/${billData.sponsors[i].url}`,
-        target: "_blank"
+        href: `https://ballotpedia.org/${billData.sponsors[i].url}`
       });
       sponsorElement.text(`${billData.sponsors[i].role} ${billData.sponsors[i].name} -- ${billData.sponsors[i].party}`);
       firstRowDiv.append(sponsorElement);
     }
-
     // console.log(billData.desc);
     // calendar
 
-    const sched = buildScheduleTable(billData.calendar);
-    const votes = buildVotes(billData.votes);
-    const descHeader = $('<div>').addClass('collapsible-header').text('Description');
-    const descBody = $('<div>').addClass('collapsible-body').text(billData.desc);
+    let sched = buildScheduleTable(billData.calendar);
+    let votes = buildVotes(billData.votes);
+    let descHeader = $('<div>').addClass('collapsible-header').text('Description');
+    let descBody = $('<div>').addClass('collapsible-body').text(billData.desc);
 
-    const schedHeader = $('<div>').addClass('collapsible-header').text('Schedule');
-    const schedBody = $('<div>').addClass('collapsible-body');
-
+    let schedHeader = $('<div>').addClass('collapsible-header').text('Schedule');
+    let schedBody = $('<div>').addClass('collapsible-body');
     schedBody.append(sched);
 
-    const voteHeader = $('<div>').addClass('collapsible-header').text('Votes');
-    const voteBody = $('<div>').addClass('collapsible-body');
+    let voteHeader = $('<div>').addClass('collapsible-header').text('Votes');
+    let voteBody = $('<div>').addClass('collapsible-body');
 
     voteBody.append(votes);
 
@@ -286,15 +263,16 @@ $('document').ready(() => {
     buttonContainer.append(exitButton, fullTextLink);
     modalContent.append(firstRowDiv, detailRowDiv, buttonContainer);
 
-    exitButton.click(() => {
+    exitButton.click(function(){
       modalContent.parent().modal('close');
-    });
+    })
   };
 
   // ========================= PROGRAM STARTS HERE ======================== //
 
   // HTML ELEMENT declarations
   const $nameInput = $('#name-input');
+  const $idInput = $('#id-input');
   const $searchButton = $('button[type="submit"]');
 
   // api info
@@ -304,24 +282,24 @@ $('document').ready(() => {
 
   // console.log($searchButton,$nameInput,$idInput);
 
-  $('.option').click((e) => {
-    const tempText = $(e.target).text();
-
+  $('.option').click((e)=>{
+    console.log($(e.target).text());
+    let tempText = $(e.target).text();
     $('.dropdown-button').text(tempText);
-  });
+    console.log($(e.target).data());
+  })
 
   $searchButton.click((e) => {
-    e.preventDefault();
-    $('#main').animate({
-      height: '150'
-    }, 500);
-    $('#title-text').fadeOut();
+    $("#main").animate({
+      "height" : "150"
+     }, 500);
+     $('#title-text').fadeOut();
 
     // reset search options
     options = '&op=search&state=CO&query=';
+    e.preventDefault();
     initSearchResults();
     $('#flavor-section').hide();
-
-    // console.log("the search results", searchResults);
+    //console.log("the search results", searchResults);
   });
 });
